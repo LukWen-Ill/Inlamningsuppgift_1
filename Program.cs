75b9acc7f95366491b627a1f12b43a53b5f13767
LukWen-Ill <Lukaswennstrom@hotmail.com>
2025-09-29 10:15:04 +0200

    Added menu logic if user is logged in

--- Program.cs
+++ Program.cs
@@ -15,13 +15,6 @@
         A user needs to be able to register an account
         A user needs to be able to log in.
         A user needs to be able to log out.
-        A user needs to be able to upload information about the item they wish to trade.
-        A user needs to be able to browse a list of other users items.
-        A user needs to be able to request a trade for other users items.
-        A user needs to be able to browse trade requests.
-        A user needs to be able to accept a trade request.
-        A user needs to be able to deny a trade request.
-        A user needs to be able to browse completed requests.
 */
 using App;
 
@@ -30,10 +23,11 @@ try { Console.Clear(); } catch { }
 
 //Uppstarts variabler
 List<User> users = new List<User>(); //Skapar en lista med users från klassen User
+List<Item> items = new List<Item>(); //Skapar en lista med users från klassen User
 User? active_user = null;
 bool running = true;
-// string input_username;
-// string input_password;
+
+
 
 // Hårdkodade användare 
 users.Add(new User("LUKAS", "LÖSENORD"));
@@ -43,68 +37,19 @@ users.Add(new User("max", "123")); // Varför funkar inte siffror?
 
 while (running)
 {
-    // if (active_user == null)
     {
         try { Console.Clear(); } catch { } // aktivera när felhanteringen är klar
         Console.WriteLine("Welcome to TRADE SYSTEM");
         Console.WriteLine("Login or Create Account");
-        Console.WriteLine("1. Login");
-        Console.WriteLine("2. Create Account");
-        if (active_user != null) { Console.WriteLine("Logout"); }
+        Console.WriteLine("1. Create Account");
+        if (active_user == null) { Console.WriteLine("2. Login"); }
+        else { Console.WriteLine("2. Trade"); }
+        if (active_user != null) { Console.WriteLine("Logout"); } // snygg men onödig 
         string input = Console.ReadLine();
         try { Console.Clear(); } catch { }
         switch (input)
         {
-            case "1": // Login
-                      //  Körs om active_user = null (EJ inloggad)
-                if (active_user == null)
-                {
-                    Console.WriteLine("--- Log in ---");
-                    Console.Write("Please Enter Email: ");
-                    string input_login_u = Console.ReadLine();
-                    Console.Write("Please Enter Password: ");
-                    string input_login_p = Console.ReadLine();
-
-                    //Try Login med input
-                    foreach (User user in users)
-                    {
-                        Console.WriteLine(user.TryLogin(input_login_u, input_login_p));
-
-                        if (user.TryLogin(input_login_u, input_login_p))
-                        {
-                            try { Console.Clear(); } catch { }
-                            Console.WriteLine("Login Successful");
-                            Console.WriteLine();
-                            Console.WriteLine("Press ENTER to continue .. ");
-                            Console.ReadLine();
-                            active_user = user;
-                            break;
-                        }
-
-                    }
-                    if (active_user == null)
-                    {
-                        try { Console.Clear(); } catch { }
-                        Console.WriteLine("Invalid login details");
-                        Console.WriteLine();
-                        Console.WriteLine("Press ENTER to continue .. ");
-                        Console.ReadLine();
-                        break;
-                    }
-                    break;
-
-                }
-                else
-                {
-                    try { Console.Clear(); } catch { }
-                    Console.WriteLine("A user is already logged in");
-                    Console.WriteLine();
-                    Console.WriteLine("Press ENTER to continue .. ");
-                    Console.ReadLine();
-                    break;
-                }
-
-            case "2": // Register Account
+            case "1": // Register Account
                 bool is_viable = true;
                 try { Console.Clear(); } catch { }
                 Console.WriteLine("--- Create Account ---");
@@ -125,7 +70,6 @@ while (running)
                 }
                 if (is_viable)
                 {
-                    // BEHÖVER BYGGAS VIDARE - BEHÖVER ETT STOP .
                     Console.Write("Please Enter Password: ");
                     string input_password = Console.ReadLine();
                     users.Add(new User(input_username, input_password));
@@ -140,13 +84,73 @@ while (running)
                     break;
                 }
                 break;
-            case "logout":
+            case "2": // Login
+                if (active_user == null)
+                {
+                    //  Körs om active_user = null (EJ inloggad)
+                    if (active_user == null)
+                    {
+                        Console.WriteLine("--- Log in ---");
+                        Console.Write("Please Enter Email: ");
+                        string input_login_u = Console.ReadLine();
+                        Console.Write("Please Enter Password: ");
+                        string input_login_p = Console.ReadLine();
+
+                        //Try Login med input
+                        foreach (User user in users)
+                        {
+                            Console.WriteLine(user.TryLogin(input_login_u, input_login_p));
+
+                            if (user.TryLogin(input_login_u, input_login_p))
+                            {
+                                try { Console.Clear(); } catch { }
+                                Console.WriteLine("Login Successful");
+                                Console.WriteLine();
+                                Console.WriteLine("Press ENTER to continue .. ");
+                                Console.ReadLine();
+                                active_user = user;
+                                break;
+                            }
+
+                        }
+                        if (active_user == null)
+                        {
+                            try { Console.Clear(); } catch { }
+                            Console.WriteLine("Invalid login details");
+                            Console.WriteLine();
+                            Console.WriteLine("Press ENTER to continue .. ");
+                            Console.ReadLine();
+                            break;
+                        }
+                        break;
+
+                    }
+                    else
+                    {
+                        try { Console.Clear(); } catch { }
+                        Console.WriteLine("A user is already logged in");
+                        Console.WriteLine();
+                        Console.WriteLine("Press ENTER to continue .. ");
+                        Console.ReadLine();
+                        break;
+                    }
+                }
+
+                //  Körs om active_user = null (EJ inloggad)
+                else
+                {
+
+                }
+            case "logout": // onödig
                 active_user = null;
                 break;
         }
     }
+    // A user needs to be able to upload information about the item they wish to trade.
+    // A user needs to be able to browse a list of other users items. item.get() + exception of exception list
+    // A user needs to be able to request a trade for other users items.
+    // A user needs to be able to browse trade requests.
+    // A user needs to be able to accept a trade request.
+    // A user needs to be able to deny a trade request.
+    // A user needs to be able to browse completed requests.
 }
-// else
-// {
-//     Console.WriteLine("LOGGAD IN");
-// }
